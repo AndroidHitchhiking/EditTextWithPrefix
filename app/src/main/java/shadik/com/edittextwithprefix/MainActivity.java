e7810dda8825a334ded240027f455ed394249cc2
Shadik <er.sadiqkhan@gmail.com>
2016-03-01 15:27:29 +0530

    EdittextWithPrefix

---
+++ app/src/main/java/shadik/com/edittextwithprefix/MainActivity.java
@@ -0,0 +1,18 @@
+package shadik.com.edittextwithprefix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditTextWithPrefix etPrefix;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etPrefix = (EditTextWithPrefix) findViewById(R.id.etPrefix);
+        etPrefix.setTag("+22");
+    }
+}
